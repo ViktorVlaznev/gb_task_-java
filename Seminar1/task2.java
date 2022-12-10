2e516e32509aad6ca99f0a060b09b75f0f1b7202
VictorVlaznev <v-vlaznev@mail.ru>
2022-12-10 21:35:50 +0300

    Add task2 in Seminar1

---
+++ Seminar1/task2.java
@@ -0,0 +1,17 @@
+// Вывести все простые числа от 1 до 1000
+
+package Seminar1;
+
+import java.math.BigInteger;
+
+public class task2 {
+    public static void main(String[] args) {
+        int maxNumber = 1000;
+        for (int i = 1; i < maxNumber; i++) {
+            Integer integer = i;
+            BigInteger bigInteger = BigInteger.valueOf(integer);
+            boolean probablePrime = bigInteger.isProbablePrime((int) Math.log(integer));
+            if (probablePrime) System.out.println(i);     
+        }       
+    }
+}
