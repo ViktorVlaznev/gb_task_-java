7bdcbc9be800f7aa0688335ef630d1af3e4c295c
VictorVlaznev <v-vlaznev@mail.ru>
2022-12-26 10:02:08 +0300

    Add task4 in Seminar5

--- Seminar5/src/main/java/org/example/Main.java
+++ Seminar5/src/main/java/org/example/Main.java
@@ -20,23 +20,26 @@
 //   Написать программу, которая найдет и выведет повторяющиеся имена с количеством повторений.
 //   Отсортировать по убыванию популярности.
 //3. Реализовать алгоритм пирамидальной сортировки (HeapSort).
-//4. На шахматной доске расставить 8 ферзей так, чтобы они не били друг друга.
 
 package org.example;
 
 import java.util.*;
 import java.util.stream.Collectors;
-
 public class Main {
     public static HashMap<String, List<String>> phoneBook = new HashMap<>();
     public static void main(String[] args) {
         // Реализуйте структуру телефонной книги с помощью HashMap, учитывая,
         // что 1 человек может иметь несколько телефонов.
-//        addInPhoneBook();
-//        findInPhoneBook("Иванова");
+        addInPhoneBook();
+        findInPhoneBook("Иванова");
+        System.out.println();
         //   Пусть дан список сотрудников:Иван Иванов
         //   Написать программу, которая найдет и выведет повторяющиеся имена с количеством повторений.
         getEmployeesNameMap();
+//      Реализовать алгоритм пирамидальной сортировки (HeapSort).
+        int[] array = {134, 65, 4, 89, 89, 2, 69, 1346, 562, 34};
+        printArray(array);
+        getHeapSort(array);
     }
 
     /**
@@ -93,5 +96,55 @@ public class Main {
                         LinkedHashMap::new
                 ));
         System.out.println(sortedMap);
+    }
+//    Реализовать алгоритм пирамидальной сортировки (HeapSort).
+    public static void sort(int arr[]) {
+        int n = arr.length;
+        // Построение кучи (перегруппируем массив)
+        for (int i = n / 2 - 1; i >= 0; i--)
+            heapify(arr, n, i);
+        // Один за другим извлекаем элементы из кучи
+        for (int i=n-1; i>=0; i--) {
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+            // Вызываем метод heapify на уменьшенной куче
+            heapify(arr, i, 0);
+        }
+    }
+
+    // Метод преобразования в двоичную кучу поддерева с корневым узлом i, что является
+    // индексом в arr[]. n - размер кучи
+    public static void heapify(int arr[], int n, int i) {
+        int largest = i; // Инициализируем наибольший элемент как корень
+        int l = 2 * i + 1; // левый = 2*i + 1
+        int r = 2 * i + 2; // правый = 2*i + 2
+        // Если левый дочерний элемент больше корня
+        if (l < n && arr[l] > arr[largest])
+            largest = l;
+        // Если правый дочерний элемент больше, чем самый большой элемент на данный момент
+        if (r < n && arr[r] > arr[largest])
+            largest = r;
+        // Если самый большой элемент не корень
+        if (largest != i) {
+            int swap = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = swap;
+            // Рекурсивно преобразуем в двоичную кучу затронутое поддерево
+            heapify(arr, n, largest);
+        }
+    }
+
+    /* Вспомогательная функция для вывода на экран массива размера n */
+    public static void printArray(int arr[]) {
+        int n = arr.length;
+        for (int i=0; i<n; ++i)
+            System.out.print(arr[i]+" ");
+        System.out.println();
+    }
+    public static void getHeapSort(int[] arr) {
+        sort(arr);
+        System.out.println("Sorted array is");
+        printArray(arr);
     }
 }
